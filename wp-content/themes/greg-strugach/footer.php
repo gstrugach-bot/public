3de3473190d5ef25c7a370aa54ca05aff181df58
Greg Strugach <gstrugach1@gmail.com>
2025-11-22 13:43:18 -0800

    I changed mind again

--- wp-content/themes/greg-strugach/footer.php
+++ wp-content/themes/greg-strugach/footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-light text-center p-3 mt-5">
-  <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. I want to change this!</p>
+  <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. I changed my mind!</p>
   <p>My Footer</p>
 </footer>
 <?php wp_footer(); ?>
