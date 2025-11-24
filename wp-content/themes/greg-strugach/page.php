a20aff53c920f3102c75865a92c886258d8cd6cf
Greg Strugach <gstrugach1@gmail.com>
2025-11-24 11:17:15 -0800

    fixed page.php page

--- wp-content/themes/greg-strugach/page.php
+++ wp-content/themes/greg-strugach/page.php
@@ -3,18 +3,16 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <article <?php post_class('mb-4'); ?>>
     <h1 class="h2"><?php the_title(); ?></h1>
-    <div class="entry-content"><?php the_content(); ?></div>
-  </article>
-<?```php
-<?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <article <?php post_class('mb-4'); ?>>
-    <h1 class="h2"><?php the_title(); ?></h1>
     <?php if ( has_post_thumbnail() ) : ?>
-      <figure class="mb-3"><?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?></figure>
+      <figure class="mb-3">
+        <?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?>
+      </figure>
     <?php endif; ?>
-    <div class="entry-content"><?php the_content(); ?></div>
+
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div>
   </article>
 <?php endwhile; endif; ?>
 
