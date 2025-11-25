4affb062f87357cfee7345dfa8a8e105c555a558
Greg Strugach <gstrugach1@gmail.com>
2025-11-25 08:12:42 -0800

    Footer Change of Nov 25

--- wp-content/themes/greg-strugach/footer.php
+++ wp-content/themes/greg-strugach/footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-light text-center p-3 mt-5">
-  <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. I changed my mind! Again and Again!</p>
+  <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. My New Project on Nov 25</p>
   <p>My Footer</p>
 </footer>
 <?php wp_footer(); ?>
